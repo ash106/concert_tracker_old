21ae92f2be82c383a6225160f472bb1a6a58e1e4
Alex Howington <alex.howington@gmail.com>
2014-04-06 16:08:55 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ConcertTracker::Application.config.secret_key_base = 'd2a55b8df370faee9c8c0394c12af4ae76d20573b17cb768fb5efd770da6d85acc1fcbcd5a38f2ee51405a56ae2a323fa79d9d94461a1c55ce5dc408769ef4e2'
