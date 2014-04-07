c2ac66fca48f99568eac8ac7e58cf5aad64f7154
Alex Howington <alex.howington@gmail.com>
2014-04-06 20:11:07 -0600

    Add date_formats initializer and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Concert.create!([
+  {
+    bands: "Pelican, Circa Survive, Thrice",
+    venue: "The Masquerade",
+    location: "Atlanta, GA",
+    date: "2008-04-26"
+  },
+  {
+    bands: "Facing New York, Emanuel, Taking Back Sunday",
+    venue: "Boulder Theatre",
+    location: "Boulder, CO",
+    date: "2006-03-31"
+  },
+  {
+    bands: "The Outline, Envy On The Coast, Your Enemies Friends, Forgive Durden, Saosin",
+    venue: "The Door",
+    location: "Dallas, TX",
+    date: "2006-08-10"
+  },
+  {
+    bands: "New London Fire, From Autumn To Ashes, Lifetime, Thursday",
+    venue: "The Starland Ballroom",
+    location: "Sayreville, NJ",
+    date: "2006-12-27"
+  },
+  {
+    bands: "Zechs Marquise, Dredg, Rx Bandits",
+    venue: "House of Blues",
+    location: "New Orleans, LA",
+    date: "2009-07-18"
+  }
+])
+    
